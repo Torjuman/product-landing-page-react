57c126bd064940dd67d66a6bb2efd83e2fcbd796
Torjuman <torjumanrimon7@gmail.com>
2023-01-31 13:05:01 +0600

    tailwindCSS setup for the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <h1>Hello World !</h1>;
-}
+const App = () => {
+  return (
+    <h1 className="text-xl font-bold text-center text-cyan-800">
+      Hello world !
+    </h1>
+  );
+};
 
 export default App;
